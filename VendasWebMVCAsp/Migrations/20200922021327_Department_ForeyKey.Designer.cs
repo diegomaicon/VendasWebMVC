03deec76fa203f6974369af39857074e807b47f9
diego maicon silva <diego_maicon20@hotmail.com>
2020-09-21 23:31:37 -0300

    Create form Simple

--- VendasWebMVCAsp/Migrations/20200922021327_Department_ForeyKey.Designer.cs
+++ VendasWebMVCAsp/Migrations/20200922021327_Department_ForeyKey.Designer.cs
@@ -2,15 +2,17 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using VendasWebMVCAsp.Models;
 
 namespace VendasWebMVCAsp.Migrations
 {
     [DbContext(typeof(VendasWebMVCAspContext))]
-    partial class VendasWebMVCAspContextModelSnapshot : ModelSnapshot
+    [Migration("20200922021327_Department_ForeyKey")]
+    partial class Department_ForeyKey
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ VendasWebMVCAsp/Services/DepartmentService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using VendasWebMVCAsp.Models;
+
+namespace VendasWebMVCAsp.Services
+{
+    public class DepartmentService
+    {
+        private readonly VendasWebMVCAspContext _context;
+        public DepartmentService(VendasWebMVCAspContext context)
+        {
+            _context = context;
+        }
+
+        public List<Department> FindAll() {
+            return _context.Department.OrderBy(x => x.Name).ToList();
+        }
+    }
+}
